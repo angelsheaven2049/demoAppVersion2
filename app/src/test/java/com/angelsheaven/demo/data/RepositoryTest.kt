46187314dc34b3b365796660348914f205ee8129
Quan Nguyen <quan.nguyen1409@gmail.com>
2019-04-12 13:34:55 +1000

    implement testing on repository

---
+++ app/src/test/java/com/angelsheaven/demo/data/RepositoryTest.kt
@@ -0,0 +1,23 @@
+package com.angelsheaven.demo.data
+
+import com.angelsheaven.demo.data.storage.StorageDataSource
+import io.reactivex.Flowable
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito
+
+class RepositoryTest {
+
+    @Test
+    fun `data source returns a value`() {
+        val ds = Mockito.mock(StorageDataSource::class.java)
+        Mockito.`when`(ds.retrieveArticleDetail(0)).then {
+            Flowable.just(Article(0))
+        }
+
+        val repository = Repository(networkDataSource = null, storageDataSource = ds)
+        assertNotNull(repository.retrieveArticleDetail(0))
+    }
+
+
+}
