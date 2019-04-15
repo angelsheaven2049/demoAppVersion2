231af78810496a2c2a9b3cf09943cb4bc47af67d
Quan Nguyen <quan.nguyen1409@gmail.com>
2019-04-15 10:26:40 +1000

    implement article testing

--- app/src/androidTest/java/com/angelsheaven/demo/data/storage/ArticleDaoTest.kt
+++ app/src/androidTest/java/com/angelsheaven/demo/data/storage/ArticleDaoTest.kt
@@ -1,11 +1,15 @@
 package com.angelsheaven.demo.data.storage
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import androidx.test.runner.AndroidJUnit4
 import com.angelsheaven.demo.data.Article
+import com.angelsheaven.demo.data.ViewModelContract
 import com.angelsheaven.demo.util.TestUtil
-import io.reactivex.Flowable
+import com.angelsheaven.demo.utilities.MyLogger
 import io.reactivex.internal.util.NotificationLite.getValue
 import junit.framework.Assert.assertNotNull
 import org.junit.Rule
@@ -13,7 +17,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ArticleDaoTest : DbTest() {
+class ArticleDaoTest : DbTest(), MyLogger {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -23,13 +27,27 @@ class ArticleDaoTest : DbTest() {
         val article = TestUtil.createArticle()
         db.articleDao().insertArticles(article)
 
-        val articleDetailQuery = DatabaseContract.SELECT_ARTICLE_DETAIL.format(0)
-        val loadedArticle = getValue<Flowable<Article>>(
+        val loadedArticle = getValue<DataSource.Factory<Int, Article>>(
             db.articleDao()
-                .getNewsDetail(SimpleSQLiteQuery(articleDetailQuery))
+                .getAllArticles()
         )
 
         assertNotNull(loadedArticle)
+
+        var formatPagingData: LiveData<PagedList<Article>>? = null
+
+        loadedArticle?.run {
+            formatPagingData = LivePagedListBuilder(this, ViewModelContract.configLoadData)
+                .setInitialLoadKey(null)
+                .build()
+
+            assertNotNull(formatPagingData)
+
+            formatPagingData?.run {
+                assertNotNull(this.value)
+            }
+        }
+
 
     }
 
